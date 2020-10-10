c3eb3edc0f5df515016084d7ddaebd11b1d970ff
Eric Fairon <eric.fairon@gmail.com>
2020-10-10 18:24:36 +0700

    1st commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module frontware.com/pwd
+
+go 1.15
+
+require (
+	github.com/google/martian v2.1.0+incompatible
+	github.com/steakknife/bloomfilter v0.0.0-20180922174646-6819c0d2a570
+	github.com/steakknife/hamming v0.0.0-20180906055917-c99c65617cd3 // indirect
+	golang.org/x/crypto v0.0.0-20201002170205-7f63de1d35b0
+)
---
+++ pwd.go
@@ -0,0 +1,76 @@
+package pwd
+
+import (
+	"bytes"
+	"crypto/rand"
+	"io"
+
+	"hash/fnv"
+
+	"github.com/steakknife/bloomfilter"
+	"golang.org/x/crypto/bcrypt"
+)
+
+//go:generate $GOPATH/bin/go-bindata -prefix "/tmp/" -nomemcopy -nometadata -nocompress -pkg pwd /tmp/pwd.bf.gz
+
+var (
+	bf *bloomfilter.Filter
+	h  = fnv.New64()
+)
+
+func init() {
+	data, _ := pwdBfGzBytes()
+	r := bytes.NewReader(data)
+	bf, _, _ = bloomfilter.ReadFrom(r) // read the BF
+}
+
+// IsCommon returns true if the password is in the list of most common passwords.
+func IsCommon(pwd string) bool {
+
+	h.Write([]byte(pwd))
+
+	return bf.Contains(h)
+}
+
+// HashPassword create a bcrypt hash
+func HashPassword(pwd string) (string, error) {
+	password := []byte(pwd)
+
+	// Hashing the password with the default cost of 10
+	hashedPassword, err := bcrypt.GenerateFromPassword(password, bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hashedPassword), nil
+}
+
+// stdChars characters to be used in auto generated password
+// We removed 1 and I to avoid confusion, sometimes they are very similar on the phone
+var stdChars = []byte("ABCDEFGHJKLMNPQRSTUVWXYZabcdefghjkmnopqrstuvwxyz23456789")
+
+// NewPassword returns a new password
+func NewPassword(length int) string {
+	return randChar(length, stdChars)
+}
+
+// randChar returns random char, usually to generate password
+func randChar(length int, chars []byte) string {
+	newPword := make([]byte, length)
+	randomData := make([]byte, length+(length/4)) // storage for random bytes.
+	clen := byte(len(chars))
+	maxrb := byte(256 - (256 % len(chars)))
+	i := 0
+	for {
+		io.ReadFull(rand.Reader, randomData)
+		for _, c := range randomData {
+			if c >= maxrb {
+				continue
+			}
+			newPword[i] = chars[c%clen]
+			i++
+			if i == length {
+				return string(newPword)
+			}
+		}
+	}
+}
